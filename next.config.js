b46d794321ede72469750c9555959e8ed7f2d158
Saya Kim-Suzuki <sayakimsuzuki@Sayas-MacBook-Air-3.local>
2025-03-24 11:53:42 -0700

    update css again

--- next.config.js
+++ next.config.js
@@ -2,9 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@radix-ui/react-icons', '@radix-ui/react-slot'],
-  experimental: {
-    optimizeCss: true,
-  }
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
